bb002373917e5b0c296940f1b06dcfd981c0d9ed
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-08-13 18:06:14 -0700

    added inline documentation for remaining files

--- Sources/NnCredentialKit/AppleSignIn/AppleSignInCoordinator.swift
+++ Sources/NnCredentialKit/AppleSignIn/AppleSignInCoordinator.swift
@@ -7,22 +7,26 @@
 
 import AuthenticationServices
 
+/// A coordinator responsible for managing the Apple Sign-In process.
 public final class AppleSignInCoordinator: NSObject {
     private var currentNonce: String?
     private var completion: ((Result<AppleCredentialInfo?, Error>) -> Void)?
     
-    public override init() { }
+    /// Initializes the coordinator.
+    public override init() {}
 }
 
 
 // MARK: - PresentationProviding
 extension AppleSignInCoordinator: ASAuthorizationControllerPresentationContextProviding {
+    /// Provides the presentation anchor for the authorization controller.
+    /// - Parameter controller: The authorization controller requesting the anchor.
+    /// - Returns: The presentation anchor to use.
     public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
               let window = windowScene.windows.first else {
             fatalError("Unexpected window scene configuration.")
         }
-        
         return window
     }
 }
@@ -30,6 +34,10 @@ extension AppleSignInCoordinator: ASAuthorizationControllerPresentationContextPr
 
 // MARK: - Delegate
 extension AppleSignInCoordinator: ASAuthorizationControllerDelegate {
+    /// Handles the completion of the authorization with the provided credentials.
+    /// - Parameters:
+    ///   - controller: The authorization controller managing the request.
+    ///   - authorization: The authorization object containing the credentials.
     public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
         do {
             if let info = try convertCredential(auth: authorization, currentNonce: currentNonce) {
@@ -40,6 +48,10 @@ extension AppleSignInCoordinator: ASAuthorizationControllerDelegate {
         }
     }
     
+    /// Handles the completion of the authorization with an error.
+    /// - Parameters:
+    ///   - controller: The authorization controller managing the request.
+    ///   - error: The error encountered during the authorization process.
     public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
         completion?(.failure(error))
     }
@@ -48,27 +60,29 @@ extension AppleSignInCoordinator: ASAuthorizationControllerDelegate {
 
 // MARK: - Handler
 public extension AppleSignInCoordinator {
+    /// Initiates the Apple Sign-In process and returns the user's credentials.
+    /// - Parameter requestedScopes: The scopes to request during sign-in. Default is `[.email, .fullName]`.
+    /// - Returns: The `AppleCredentialInfo` containing the user's credentials, or `nil` if the user cancels.
     func createAppleTokenInfo(requestedScopes: [ASAuthorization.Scope]? = [.email, .fullName]) async throws -> AppleCredentialInfo? {
         return try await withCheckedThrowingContinuation { continuation in
             completion = { result in
                 switch result {
-                case .success(let info):
-                    continuation.resume(returning: info)
+                case .success(let info): continuation.resume(returning: info)
                 case .failure(let error):
                     if let appleSignInError = error as? ASAuthorizationError, appleSignInError.code == .canceled {
-                        return continuation.resume(returning: nil)
+                        continuation.resume(returning: nil)
                     } else {
-                        return continuation.resume(throwing: error)
+                        continuation.resume(throwing: error)
                     }
                 }
             }
-
+            
             currentNonce = NonceFactory.randomNonceString()
             
             let request = ASAuthorizationAppleIDProvider().createRequest()
             request.requestedScopes = requestedScopes
             request.nonce = NonceFactory.sha256(currentNonce!)
-
+            
             let authorizationController = ASAuthorizationController(authorizationRequests: [request])
             authorizationController.delegate = self
             authorizationController.presentationContextProvider = self
@@ -80,6 +94,12 @@ public extension AppleSignInCoordinator {
 
 // MARK: - Private Methods
 private extension AppleSignInCoordinator {
+    /// Converts the authorization object into an `AppleCredentialInfo` object.
+    /// - Parameters:
+    ///   - auth: The authorization object containing the credentials.
+    ///   - currentNonce: The nonce used to verify the credentials.
+    /// - Returns: The `AppleCredentialInfo` containing the user's credentials, or `nil` if conversion fails.
+    /// - Throws: An error if the conversion fails due to invalid state or serialization issues.
     func convertCredential(auth: ASAuthorization, currentNonce: String?) throws -> AppleCredentialInfo? {
         guard let appleIDCredential = auth.credential as? ASAuthorizationAppleIDCredential else { return nil }
         guard let currentNonce = currentNonce else { throw AppleSignInError.invalidState }
@@ -89,20 +109,24 @@ private extension AppleSignInCoordinator {
         
         return .init(email: appleIDCredential.email, displayName: displayName, idTokenString: idTokenString, nonce: currentNonce)
     }
-
+    
+    /// Assembles a display name from the provided name components.
+    /// - Parameter fullName: The `PersonNameComponents` containing the user's full name.
+    /// - Returns: A `String` representing the user's full name.
     func getDisplayName(from fullName: PersonNameComponents?) -> String {
         let firstName = fullName?.givenName ?? ""
         let lastName = fullName?.familyName ?? ""
-        
         return "\(firstName) \(lastName)"
     }
-
+    
+    /// Serializes the identity token into a string.
+    /// - Parameter appleIDToken: The identity token as `Data`.
+    /// - Returns: A `String` representing the serialized token.
+    /// - Throws: An error if the token cannot be serialized.
     func serializeToken(_ appleIDToken: Data?) throws -> String {
         guard let appleIDToken = appleIDToken, let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
             throw AppleSignInError.unableToSerializeToken
         }
-        
         return idTokenString
     }
 }
-
--- Sources/NnCredentialKit/AppleSignIn/NonceFactory.swift
+++ Sources/NnCredentialKit/AppleSignIn/NonceFactory.swift
@@ -8,22 +8,25 @@
 import CryptoKit
 import AuthenticationServices
 
+/// A utility class for generating and hashing nonces used in authentication processes.
 enum NonceFactory {
+    /// Hashes the given input string using SHA-256.
+    /// - Parameter input: The input string to hash.
+    /// - Returns: The hashed string.
     static func sha256(_ input: String) -> String {
         let inputData = Data(input.utf8)
         let hashedData = SHA256.hash(data: inputData)
-        let hashString = hashedData.compactMap {
-            String(format: "%02x", $0)
-        }.joined()
-        
+        let hashString = hashedData.compactMap { String(format: "%02x", $0) }.joined()
         return hashString
     }
     
+    /// Generates a random nonce string of the specified length.
+    /// - Parameter length: The length of the nonce string. Default is 32.
+    /// - Returns: A random nonce string.
     static func randomNonceString(length: Int = 32) -> String {
         precondition(length > 0)
         
         let charset: [Character] = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
-        
         var result = ""
         var remainingLength = length
         
@@ -32,9 +35,7 @@ enum NonceFactory {
                 var random: UInt8 = 0
                 let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
                 if errorCode != errSecSuccess {
-                    fatalError(
-                        "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
-                    )
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
                 }
                 return random
             }
